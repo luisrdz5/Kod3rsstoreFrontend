386265aa75d7862400c6a6a9da3d4e05527f2c34
luisrdz5 <luis_rdz5@hotmail.com>
2020-06-09 14:57:21 -0500

    generated the vendor file

--- src/SSR-server/server.js
+++ src/SSR-server/server.js
@@ -52,6 +52,7 @@ if(ENV === 'development'){
 const setResponse = (html, preloadedState, manifest) => {
   const mainStyles = manifest ? manifest['main.css'] : 'assets/app.css';
   const mainBuild = manifest ? manifest['main.js'] : 'assets/app.js';
+  const vendorBuild = manifest ? manifest['vendors.js'] : 'assets/vendor.js';
 
 
   return (`
@@ -72,6 +73,7 @@ const setResponse = (html, preloadedState, manifest) => {
           window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}
         </script>
         <script src="${mainBuild}" type="text/javascript"> </script>
+        <script src="${vendorBuild}" type="text/javascript"> </script>
         <script src="https://kit.fontawesome.com/473d269aa9.js"></script>
       </body>
     </html>
